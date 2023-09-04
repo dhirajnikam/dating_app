1abbf55fc64b797cab6d51e7fc2b973c30ad424b
dhirajnikam <dhirajnikam78@gmail.com>
2023-09-04 11:28:10 +0530

    first commit

---
+++ lib/widgets/cards_stack_widget.dart
@@ -0,0 +1,276 @@
+import 'package:flutter/material.dart';
+import 'package:dating_app/model/profile.dart';
+import 'package:dating_app/widgets/action_button_widget.dart';
+import 'package:dating_app/widgets/drag_widget.dart';
+import 'package:get/get.dart';
+import 'package:get/utils.dart';
+
+enum Swipe { none, left, right, top, bottom }
+
+class CardsStackWidget extends StatefulWidget {
+  const CardsStackWidget({Key? key}) : super(key: key);
+
+  @override
+  State<CardsStackWidget> createState() => _CardsStackWidgetState();
+}
+
+class _CardsStackWidgetState extends State<CardsStackWidget>
+    with SingleTickerProviderStateMixin {
+  List<Profile> draggableItems = [
+    const Profile(
+        name: 'Rohini',
+        distance: '10 miles away',
+        imageAsset: 'assets/images/avatar_1.png'),
+    const Profile(
+        name: 'Rohini',
+        distance: '10 miles away',
+        imageAsset: 'assets/images/avatar_2.png'),
+    const Profile(
+        name: 'Rohini',
+        distance: '10 miles away',
+        imageAsset: 'assets/images/avatar_3.png'),
+    const Profile(
+        name: 'Rohini',
+        distance: '10 miles away',
+        imageAsset: 'assets/images/avatar_4.png'),
+    const Profile(
+        name: 'Rohini',
+        distance: '10 miles away',
+        imageAsset: 'assets/images/avatar_5.png'),
+  ];
+
+  ValueNotifier<Swipe> swipeNotifier = ValueNotifier(Swipe.none);
+  late final AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+    _animationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        draggableItems.removeLast();
+        _animationController.reset();
+
+        swipeNotifier.value = Swipe.none;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: ValueListenableBuilder(
+            valueListenable: swipeNotifier,
+            builder: (context, swipe, _) => Stack(
+              clipBehavior: Clip.none,
+              alignment: Alignment.center,
+              children: List.generate(draggableItems.length, (index) {
+                if (index == draggableItems.length - 1) {
+                  return PositionedTransition(
+                    rect: RelativeRectTween(
+                      begin: RelativeRect.fromSize(
+                          const Rect.fromLTWH(0, 0, 580, 340),
+                          const Size(580, 340)),
+                      end: RelativeRect.fromSize(
+                          Rect.fromLTWH(
+                              swipe != Swipe.none
+                                  ? swipe == Swipe.left
+                                      ? -300
+                                      : swipe == Swipe.right
+                                          ? 300
+                                          : 0
+                                  : 0,
+                              swipe == Swipe.bottom ? 700 : 0,
+                              580,
+                              340),
+                          const Size(580, 340)),
+                    ).animate(CurvedAnimation(
+                      parent: _animationController,
+                      curve: Curves.easeInOut,
+                    )),
+                    child: RotationTransition(
+                      turns: Tween<double>(
+                              begin: 0,
+                              end: swipe != Swipe.none
+                                  ? swipe == Swipe.left
+                                      ? -0.1 * 0.3
+                                      : swipe == Swipe.right
+                                          ? 0.1 * 0.3
+                                          : 0.0
+                                  : 0.0)
+                          .animate(
+                        CurvedAnimation(
+                          parent: _animationController,
+                          curve:
+                              const Interval(0, 0.4, curve: Curves.easeInOut),
+                        ),
+                      ),
+                      child: GestureDetector(
+                        onVerticalDragEnd: (details) {
+                          if (details.velocity.pixelsPerSecond.dy > 1000) {
+                            // Swipe down (bottom)
+                            _animationController.forward();
+                            swipeNotifier.value = Swipe.bottom;
+                          } else if (details.velocity.pixelsPerSecond.dy <
+                              -1000) {
+                            // Swipe up (top)
+                            _animationController.forward();
+                            swipeNotifier.value = Swipe.top;
+                          }
+                        },
+                        child: DragWidget(
+                          profile: draggableItems[index],
+                          index: index,
+                          swipeNotifier: swipeNotifier,
+                        ),
+                      ),
+                    ),
+                  );
+                } else {
+                  return DragWidget(
+                    profile: draggableItems[index],
+                    index: index,
+                    swipeNotifier: swipeNotifier,
+                  );
+                }
+              }),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 10,
+          left: 0,
+          right: 0,
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 46.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ActionButtonWidget(
+                  onPressed: () {
+                    swipeNotifier.value = Swipe.left;
+                    _animationController.forward();
+                  },
+                  icon: const Icon(
+                    Icons.close,
+                    color: Colors.grey,
+                  ),
+                ),
+                const SizedBox(width: 20),
+                ActionButtonWidget(
+                  onPressed: () {
+                    swipeNotifier.value = Swipe.right;
+                    _animationController.forward();
+                  },
+                  icon: const Icon(
+                    Icons.favorite,
+                    color: Colors.red,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          left: 30,
+          child: DragTarget<int>(
+            builder: (
+              BuildContext context,
+              List<dynamic> accepted,
+              List<dynamic> rejected,
+            ) {
+              return IgnorePointer(
+                child: Container(
+                  height: 700.0,
+                  width: 80.0,
+                  color: Colors.transparent,
+                ),
+              );
+            },
+            onAccept: (int index) {
+              setState(() {
+                draggableItems.removeAt(index);
+              });
+            },
+          ),
+        ),
+        Positioned(
+          top: 0,
+          child: DragTarget<int>(
+            builder: (
+              BuildContext context,
+              List<dynamic> accepted,
+              List<dynamic> rejected,
+            ) {
+              return IgnorePointer(
+                child: Container(
+                  height: 700.0,
+                  width: Get.width * 0.9,
+                  color: Colors.transparent,
+                ),
+              );
+            },
+            onAccept: (int index) {
+              setState(() {
+                draggableItems.removeAt(index);
+              });
+            },
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          child: DragTarget<int>(
+            builder: (
+              BuildContext context,
+              List<dynamic> accepted,
+              List<dynamic> rejected,
+            ) {
+              return IgnorePointer(
+                child: Container(
+                  height: 700.0,
+                  width: Get.width * 0.9,
+                  color: Colors.transparent,
+                ),
+              );
+            },
+            onAccept: (int index) {
+              setState(() {
+                draggableItems.removeAt(index);
+              });
+            },
+          ),
+        ),
+        Positioned(
+          right: 30,
+          child: DragTarget<int>(
+            builder: (
+              BuildContext context,
+              List<dynamic> accepted,
+              List<dynamic> rejected,
+            ) {
+              return IgnorePointer(
+                child: Container(
+                  height: 700.0,
+                  width: 80.0,
+                  color: Colors.transparent,
+                ),
+              );
+            },
+            onAccept: (int index) {
+              setState(() {
+                draggableItems.removeAt(index);
+              });
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
